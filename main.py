cb614145dce0579b75d90535f2aafc76faa8f93e
Galia101 <galiya.almejrin@gmail.com>
2023-08-28 21:02:26 +0300

    Galia1012

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# write your code here
+# write your code her
+favorit_animals = ["dog" ,'cat' , "monkey" , "rabbit"]
+print("favorit_animals")
+print(favorit_animals[1])
+favorit_animals.remove("monkey")
+favorit_animals.insert(1, "bat")
+for animal in favorit_animals:
+    print(f"I Love {animal} ")
+numbers = [5,4,3,2,1,]
+squares = 0
+for num in numbers :
+    print =[25,16,9,4,1]
